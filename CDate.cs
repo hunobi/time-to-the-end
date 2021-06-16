148addba2627a91f916ce85142a4f4d2deeba336
hunobi <74296003+hunobi@users.noreply.github.com>
2021-06-16 16:05:21 +0200

    v1.0.0

---
+++ CDate.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatyCzas
+{
+    class CDate
+    {
+
+        public CDate(string startTime, string endTime)
+        {
+            TimeToHome = DateTime.Parse(DateTime.Now.ToShortDateString() + " " + endTime);
+            StartTime = DateTime.Parse(DateTime.Now.ToShortDateString() + " " + startTime);
+            TimeToHomeTicks = TimeToHome.Ticks;
+            DayInWeek = new Dictionary<string, int>();
+            DayInWeek.Add("Monday", 1);
+            DayInWeek.Add("Tuesday", 2);
+            DayInWeek.Add("Wednesday", 3);
+            DayInWeek.Add("Thursday", 4);
+            DayInWeek.Add("Friday", 5);
+            DayInWeek.Add("Saturday", 6);
+            DayInWeek.Add("Sunday", 7);
+        }
+
+        private Dictionary<string, int> DayInWeek { get; set; }
+        private DateTime StartTime { get; set; }
+        private TimeSpan Time { get; set; }
+        private DateTime TimeToHome { get; set; }
+        private long TimeToHomeTicks { get; set; }
+        private string CorrectTime(int time)
+        {
+            if (time < 10)
+            {
+                return "0" + time;
+            }
+            else
+            {
+                return time.ToString();
+            }
+        }
+
+        public long GetTick()
+        {
+            return TimeToHomeTicks - DateTime.Now.Ticks;
+        }
+
+        public float GetPercent()
+        {
+            long currentTime = (((DateTime.Now.Hour * 60) + DateTime.Now.Minute) * 60) + DateTime.Now.Second;
+            long finalTime = (((TimeToHome.Hour * 60) + TimeToHome.Minute) * 60) + TimeToHome.Second;
+            long startTime = (((StartTime.Hour * 60) + StartTime.Minute) * 60) + StartTime.Second;
+            return (currentTime - startTime) / ((float)(finalTime - startTime)) * 100; 
+        }
+
+        public string GetWeekendPercent()
+        {
+            string result = "";
+            string today = DateTime.Now.DayOfWeek.ToString();
+
+            foreach(var key in DayInWeek)
+            {
+                if(key.Key == today)
+                {
+                    double tmp = Math.Round(key.Value * 100 / 5f, 2);
+                    if(tmp > 100)
+                    {
+                        result = "Weekend";
+                    }
+                    else if(tmp == 100)
+                    {
+                        result = "Ostatni dzień!";
+                    }
+                    else
+                    {
+                        result = tmp.ToString() + "%";
+                    }
+                    break;
+                }
+            }
+            return result;
+        }
+
+        public string GetTimeToEndWork()
+        {
+            Time = TimeSpan.FromTicks(TimeToHomeTicks - DateTime.Now.Ticks);
+            return String.Format("{0}:{1}:{2}", CorrectTime(Time.Hours), CorrectTime(Time.Minutes), CorrectTime(Time.Seconds));
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,122 @@
+﻿using DatyCzas;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace CzasDoKonca
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ShowTime.Visibility = Visibility.Hidden;
+        }
+
+        private Task Process;
+        private CDate Date;
+
+        private void Calculate()
+        {
+            while (Date != null && Date.GetTick() > 0)
+            {                        
+                Dispatcher.Invoke(() =>
+                {
+                    Time.Content = Date.GetTimeToEndWork();
+                    Percent.Content = Math.Round(Date.GetPercent(),2) + "%";
+                    Weekend.Content = Date.GetWeekendPercent();
+                   
+                });
+                Thread.Sleep(500);
+            }
+            Dispatcher.Invoke(() =>
+            {
+                if (Date != null && Date.GetTick() <= 0)
+                {
+                    Time.Content = "00:00:00";
+                    Percent.Content = "100%";
+                    Weekend.Content = Date.GetWeekendPercent();
+                }
+            });          
+        }
+
+        private void Button_Start(object sender, RoutedEventArgs e)
+        {
+            if (isGood(StartTime.Text))
+            {
+                StartTime.BorderBrush = Brushes.Black;
+                if (isGood(EndTime.Text))
+                {
+                    EndTime.BorderBrush = Brushes.Black;
+                    switch (MainButton.Content)
+                    {
+                        case "Start":
+                            ShowTime.Visibility = Visibility.Visible;
+                            Date = new CDate(StartTime.Text, EndTime.Text);
+                            Process = new Task(Calculate);                           
+                            Process.Start();
+                            MainButton.Content = "Stop";
+                            break;
+                        case "Stop":
+                            Process = null;
+                            Date = null;
+                            MainButton.Content = "Start";
+                            break;
+                    }
+                }
+                else
+                {
+                    EndTime.BorderBrush = Brushes.Red;
+                }
+            }
+            else
+            {
+                StartTime.BorderBrush = Brushes.Red;
+            }                 
+        }
+
+        private bool isGood(string time)
+        {
+            var tmp = time.Split(':');
+            if(tmp.Length == 3)
+            {
+                for(int i = 0; i < tmp.Length; i++)
+                {
+                    try
+                    {
+                        int n = Convert.ToInt16(tmp[i]);
+                        switch (i)
+                        {
+                            case 0:
+                                if(n < 0 || n > 23)
+                                {
+                                    return false;
+                                }
+                                break;
+                            default:
+                                if (n < 0 || n > 59)
+                                {
+                                    return false;
+                                }
+                                break;
+                        }
+                    }
+                    catch
+                    {
+                        return false;
+                    }
+                }
+            }
+            else
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
